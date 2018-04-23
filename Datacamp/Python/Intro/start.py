c67a394c8ccf0242064af1615c43b233dee65253
awesome-numberon <tiwarivivekraj.16je002188@cse.ism.ac.in>
2018-04-23 23:48:25 +0530

    1

---
+++ Datacamp/Python/Intro/start.py
@@ -0,0 +1,25 @@
+# Starting with python
+'''
+PYTHON as a calculator
+
+'''
+#It can perform various operations like + , * , - , /
+
+print('Add:',5+5)
+print('Subtract:',5-5)
+
+print('Multiply:',5*5)
+print('Divide:',5/5)
+
+#Exponentiation and modulo
+
+#Suppose you deposited $100 and interest rate is 1.1 . Therefore money after 7 yrs?
+x = 1.1**7
+print('Total money: $',x*100)
+
+print('Modulo:',15%8)
+
+'''
+    What we can do with python( Basic Operations )
+    Precedence : ** ,( % , / , * ),( + , - )
+'''
