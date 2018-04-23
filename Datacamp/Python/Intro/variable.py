7881789706bcbc5b8f78f887cdbc8cdc929ced4b
awesome-numberon <tiwarivivekraj.16je002188@cse.ism.ac.in>
2018-04-23 23:37:59 +0530

    Got started

---
+++ Datacamp/Python/Intro/variable.py
@@ -0,0 +1,53 @@
+# Creating Variable
+
+# Create a variable savings
+savings = 100        # $100
+
+# Create a variable factor
+factor = 1.10
+
+# Calculate result
+result = savings * factor**7
+
+# Print out result
+print(result)
+# Create a variable desc
+desc = 'compound interest'
+
+# Create a variable profitable
+profitable = True
+
+print('Typr of variable profitable: ',type(profitable))
+print('Type of variable desc: ',type(desc))
+
+#OPERATOR WITH MULTIPLE MEANINGS
+# Several variables to experiment with
+savings = 100
+factor = 1.1
+desc = "CompoundInterest"
+
+# Assign product of factor and savings to year1
+year1 = savings*factor
+
+# Print the type of year1
+print('Type of year1: ',type(year1))
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+# Print out doubledesc
+print(doubledesc)
+
+# Definition of savings and result     TYPE CONVERSIONS
+savings = 100
+result = 100 * 1.1 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+'''
+    Creating the variable , performing operations on them , accessing the type of the variable , type conversion.
+'''
