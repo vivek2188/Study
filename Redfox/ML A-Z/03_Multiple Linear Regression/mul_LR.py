45adeb84c2f9d73487072492cdc0a5d5a6d35360
vivek2188 <tiwarivivekraj17@gmail.com>
2018-05-06 04:02:03 +0530

    Multiple Linear Regression - Backward Elimination Technique

--- Redfox/ML A-Z/03_Multiple Linear Regression/mul_LR.py
+++ Redfox/ML A-Z/03_Multiple Linear Regression/mul_LR.py
@@ -35,17 +35,20 @@ import statsmodels.formula.api as sm
 X = np.append(arr=np.ones((50,1)).astype(int),values=X,axis=1) # 1 for column
 X_opt = X[:,[0,1,2,3,4,5]]
 regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()    #Ordinary Least Squares
-regressor_ols.summary()
-X_opt = X[:,[0,1,3,4,5]]
-regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()    
-regressor_ols.summary()
-X_opt = X[:,[0,3,4,5]]
-regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()
-regressor_ols.summary()
-X_opt = X[:,[0,3,5]]
-regressor_ols = sm.OLS(endog=y,exog=X_opt).fit() 
-regressor_ols.summary()
-X_opt = X[:,[0,3]]
-regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()  
-regressor_ols.summary()
+print(regressor_ols.summary())
 
+X_opt = X[:,[0,1,3,4,5]]
+regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()    #Ordinary Least Squares
+print(regressor_ols.summary())
+
+X_opt = X[:,[0,3,4,5]]
+regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()    #Ordinary Least Squares
+print(regressor_ols.summary())
+
+X_opt = X[:,[0,3,5]]
+regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()    #Ordinary Least Squares
+print(regressor_ols.summary())
+
+X_opt = X[:,[0,3]]
+regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()    #Ordinary Least Squares
+print(regressor_ols.summary())
