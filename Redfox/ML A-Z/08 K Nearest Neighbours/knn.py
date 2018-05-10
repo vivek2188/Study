b70cd3e1af79b439b1cbcc158aedf679ed9f1b3e
vivek2188 <tiwarivivekraj17@gmail.com>
2018-05-10 16:12:49 +0530

    KNN

--- Redfox/ML A-Z/08 K Nearest Neighbours/knn.py
+++ Redfox/ML A-Z/08 K Nearest Neighbours/knn.py
@@ -7,11 +7,10 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('Social_Network_Ads.csv')
 X = data.iloc[:,2:4].values
 y = data.iloc[:,-1].values
-#y = np.reshape(y,newshape=((len(y),1)))
 
-#Splitting the dataset int train and test set
+#Splitting the dataset into train and test set
 from sklearn.cross_validation import train_test_split
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0) 
 
 #Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -19,18 +18,19 @@ scl_x = StandardScaler()
 X_train = scl_x.fit_transform(X_train)
 X_test = scl_x.transform(X_test)
 
-#Fitting Logistic Regression to our training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state=0)
-classifier.fit(X_train,y_train)
+#Fitting the KNN to the training set
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
+knn.fit(X_train,y_train)
 
-#Predicting te Test set results
-y_pred = classifier.predict(X_test)
-#y_pred = np.reshape(y_pred,newshape=((len(y_pred),1)))
+#Predicting for the test set
+y_pred = knn.predict(X_test)
 
+ 
 #Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
+print('Confusion Matrix:')
 print(cm)
 
 #Visualising the Training Set Results
@@ -38,14 +38,14 @@ from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+plt.contourf(X1, X2, knn.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression Training Set')
+plt.title('KNN Training Set')
 plt.ylabel('Estimated Salary')
 plt.xlabel('Age')
 plt.legend()
@@ -56,14 +56,14 @@ from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+plt.contourf(X1, X2, knn.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression Test Set')
+plt.title('KNN Test Set')
 plt.ylabel('Estimated Salary')
 plt.xlabel('Age')
 plt.legend()
