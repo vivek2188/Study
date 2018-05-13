1a5a723f89127ed2bba410ca8b1096fe3442ff9b
vivek2188 <tiwarivivekraj17@gmail.com>
2018-05-13 17:04:12 +0530

    SVM - Guassian Kernel

---
+++ Redfox/ML A-Z/10 Kernel SVM/ksvm.py
@@ -0,0 +1,69 @@
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing the dataset
+data = pd.read_csv('Social_Network_Ads.csv')
+X = data.iloc[:,2:4].values
+y = data.iloc[:,-1].values
+
+#Splitting the dataset
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+scl_x = StandardScaler()
+X_train = scl_x.fit_transform(X_train)
+X_test = scl_x.transform(X_test)
+
+#Fitting the model to the dataset
+from sklearn.svm import SVC
+svm = SVC(kernel='rbf',random_state=0)
+svm.fit(X_train,y_train)
+
+#Predicting the result
+y_pred = svm.predict(X_test)
+
+#Making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print('Confusion Matrix: ')
+print(cm)
+
+#Visualising the Training Set Results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, svm.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM Training Set')
+plt.ylabel('Estimated Salary')
+plt.xlabel('Age')
+plt.legend()
+plt.show()
+
+#Visualising the Test Set Results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, svm.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM Test Set')
+plt.ylabel('Estimated Salary')
+plt.xlabel('Age')
+plt.legend()
+plt.show()
