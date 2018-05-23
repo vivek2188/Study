b56ac76bb7e936037d868a7c0ef3c77089a9d7e1
vivek2188 <tiwarivivekraj17@gmail.com>
2018-05-23 22:36:09 +0530

    KMeans Intro

---
+++ Sentdex/03 Clustering/km1.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+from sklearn.cluster import KMeans
+
+X = np.array([[1,2],[1.5,1.8],[5,8],[8,8],[1,0.6],[9,11]])
+
+#plt.scatter(X[:,0],X[:,1],s=150)
+#plt.show()
+
+clf = KMeans(n_clusters=2)
+clf.fit(X)
+centroids = clf.cluster_centers_
+labels =  clf.labels_
+
+colors = ['red','blue','green','k','magenta']
+
+for i in range(len(X)):
+    plt.plot(X[i,0],X[i,1],marker='o',color=colors[labels[i]],markersize=10)
+plt.scatter(centroids[:,0],centroids[:,1],marker='x',s=150,linewidth=5)
+plt.show()
