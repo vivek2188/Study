8e55ae75296dc5503e540a876a057c2d11c1d4e5
vivek2188 <tiwarivivekraj17@gmail.com>
2018-08-09 22:33:24 +0530

    bfs

--- C++ Programming/dfs.cpp
+++ C++ Programming/dfs.cpp
@@ -1,8 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
 void dfs(int s, vector<int> g[], bool vis[]){
     vis[s] = true;
     cout << s << " ";
     for(int i=0;i<g[s].size();i++){
         if(!vis[g[s][i]])
             dfs(g[s][i],g,vis);
+    }
+}
+
+void bfs(int s,vector<int> adj[],bool vis[]){
+    queue<int>q;
+    vis[s] = true;
+    q.push(s);
+    while(!q.empty()){
+        cout << q.front() << " ";
+        int k = q.front();
+        q.pop();
+        for(int i=0;i<g[k].size();i++){
+            if(!vis[g[k][i]]){
+                q.push(g[k][i]);
+                vis[g[k][i]] = true;   
+            }
+        }
     }
 }
