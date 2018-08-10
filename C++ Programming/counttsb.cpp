a9425f2d86997a70e28e7685e16623fa2f89f6ea
vivek2188 <tiwarivivekraj17@gmail.com>
2018-08-10 15:12:37 +0530

    Faster method

--- C++ Programming/counttsb.cpp
+++ C++ Programming/counttsb.cpp
@@ -1,3 +1,5 @@
+// O(n) Time Complexity
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -21,5 +23,39 @@ int main(){
             count += getbits(i);
         }
         cout << count << "\n";
+    }
+}
+*/
+
+// Much faster algorithm with O(logN) time Complexity
+#include <bits/stdc++.h>
+using namespace std;
+
+int getbits(int n){
+    int cnt = 0;
+    while(n>0){
+        cnt ++;
+        n = n - (n&-n);
+    }
+    return cnt;
+}
+
+int count_bits(int n){
+    if(n<=0)
+        return 0;
+    if((n&(n+1))==0){
+        return log2(n+1) * (n+1)/2;
+    }
+    int m = pow(2,int(log2(n))) - 1;
+    return count_bits(m) + count_bits(n-m-1) + n-m; 
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n; 
+        cin >> n;
+        cout << count_bits(n) << "\n";
     }
 }
