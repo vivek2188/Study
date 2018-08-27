3eb42fc59a94aa2e44bab993c71ffc1adf50596b
vivek2188 <tiwarivivekraj17@gmail.com>
2018-08-27 23:34:07 +0530

    Process Concurency and CPU Scheduling

--- Operating Systems/UNIX Multiprocess Programming/Fork/fork.c
+++ Operating Systems/UNIX Multiprocess Programming/Fork/fork.c
@@ -5,7 +5,12 @@
 #include <string.h>
 
 #define MAX 100
-
+#define MAX_COUNT 200
+/*
+ If fork() returns a negative value, the creation of child process was unsuccessful.
+ Returns zero to the newly created child process.
+ Returns positive value to the parent.
+*/
 int main(){
 	pid_t pid;
 	fork();
@@ -14,7 +19,7 @@ int main(){
 	char output_string[MAX];
 
 	int i;
-	for(i=0;i<5;i++){
+	for(i=0;i<MAX_COUNT;i++){
 		//printf("Process ID: %d Int: %d\n",pid,i);
 		sprintf(output_string,"Process ID: %d Int: %d\n",pid,i);
         	// First argument denotes the file descriptor(fd)
@@ -29,6 +34,11 @@ int main(){
  screen immediately, you may not get the right order of the expected
  result. Worse, the output from the two processes may be mixed in
  strange ways. To overcome this problem, you may consider to use the "unbuffered" write.
+
+ Both the processes run concurrently, the output lines are mixed in a
+ rather unpredictable way. Moreover, the order of these lines are
+ determined by the CPU Scheduler. Hence, if you run this program again,
+ you may get a totally different result.
 */
 
 	}
