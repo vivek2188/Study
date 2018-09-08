84be7ef2148a4042ead9d0a964a0f09e16b4c6f5
vivek2188 <tiwarivivekraj17@gmail.com>
2018-09-08 12:39:15 +0530

    Optimized naive when pattern consists of distinct characters

---
+++ Algorithm/String Algorithms/Naive/naive-with-different-char.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string text, pattern;
+	cout << "Enter text and pattern: ";
+	cin >> text >> pattern;
+	int n = text.length();
+	int m = pattern.length();
+	if(m>n){
+		cout << "Error\n";
+		exit(true);	
+	}
+	// ALgorithm for pattern with distinct characters - O(n)
+	text = "#" + text;
+	for(int s=0;s<=n-m;){	
+		int i=1;
+		while(i<=m and text[s+i]==pattern[i-1])
+			i++;
+		if(i==m+1)
+			cout << "Pattern occurs with shift " << s << "\n";
+		if(i==1)
+			s = s+1;
+		else 
+			s = s+i-1;
+	}
+	return 0;
+}
