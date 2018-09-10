b3eaaa68c71d945c176a650968c62b3f4e1cbdeb
vivek2188 <tiwarivivekraj17@gmail.com>
2018-09-10 12:55:22 +0530

    Hits count

--- Algorithm/String Algorithms/Rabin-Karp/rabin-karp.cpp
+++ Algorithm/String Algorithms/Rabin-Karp/rabin-karp.cpp
@@ -3,10 +3,9 @@
 #include <math.h>
 using namespace std;
 
-void rabin_karp_string_matcher(string T,string P,int q){
+void rabin_karp_string_matcher(string T,string P,int q,int d){
 	int n = T.length();
 	int m = P.length();
-	int d = 10;	      // Assuming string is in radix-10 notation
 	if(m>n){
 		cout << "Error\n";
 		exit(true);
@@ -22,18 +21,22 @@ void rabin_karp_string_matcher(string T,string P,int q){
 	}
 	//Algorithm
 	int h = pow(d,m-1);	// h = d^(m-1)
-	int spurious_hits = 0;
+	int spurious_hits, valid, invalid;
+	spurious_hits = valid = invalid = 0;
 	int compute[n-m+1];
 	compute[0] = t;
 	for(int s=0;s<=n-m;s++){
 		if(t==p){
 			int i=1;
-			while(T[s+i]==P[i])
+			while(T[s+i]==P[i] and i<=m)
 				i++;
-			if(i==m+1)
+			if(i==m+1){
 				cout << "Pattern occurs with shift " << s << "\n";
+				valid++;
+			}
 			else spurious_hits++;
 		}
+		else invalid++;
 		if(s<n-m){
 			t = (d * (t - (T[s+1]-'0')*h) + T[s+m+1]-'0') % q;
 			if(t<0)
@@ -45,15 +48,17 @@ void rabin_karp_string_matcher(string T,string P,int q){
 		cout << compute[i] << " ";
 	cout << "\n";
 	cout << "Number of spurious hits: " << spurious_hits << "\n";
+	cout << "Number of valid hits: " << valid << "\n";
+	cout << "Number of invalid hits: " << invalid << "\n";
 }
 
 int main(){
 	string text, pattern;
 	cout << "Enter text and pattern: ";
 	cin >> text >> pattern;
-	int q;
-	cout << "Enter the prime number: ";
-	cin >> q;
-	rabin_karp_string_matcher(text,pattern,q);
+	int q,d;
+	cout << "Enter the prime and radix number: ";
+	cin >> q >> d;
+	rabin_karp_string_matcher(text,pattern,q,d);
 	return 0;
 }
