fab5aa9abe15759ff4307559ad3023db77cb2153
vivek2188 <tiwarivivekraj17@gmail.com>
2018-09-11 11:08:43 +0530

    minor

--- Algorithm/String Algorithms/Finite-Automata/finite-automata.cpp
+++ Algorithm/String Algorithms/Finite-Automata/finite-automata.cpp
@@ -4,6 +4,37 @@
 #include <map>
 using namespace std;
 
+map<pair<int,char>,int> compute_transition_function(string P,int z){
+	/*
+	  Arguments: 
+		string P		            Pattern Text String
+		int z				    Size of the alphabet
+		RETURNS				    Transition Function
+	*/
+	map<pair<int,char>,int> mp;
+	/*
+	  q represents the number of character in string matched.
+	  For computing transition(q,a):
+		If P[q+1] = a then
+			transition(q,a) = q + 1
+		Otherwise
+			transition(q,a) = sigma(p(r)a) 
+		p(r) is the longest prefix in p(q) which is also a suffix such that r < q.
+	*/
+	int q = 0;
+	while(q<=m){
+		for(char i=0;i<z;i++){
+			char a = 'a'+z;
+			// Compute transtion(q,a)
+			if(P[q+1]==a and q!=m)
+				mp[make_pair(q,a)] = q+1;
+			else
+				mp[make_pair(q,a)] = mp[make_pair(pi[q],a)];
+		}
+	}
+	return mp;
+}
+
 void finite_automata_matcher(string T,map<pair<int,char>,int>transition,int m,int z){
 	/*
 	  Arguments: 
