fd7bae572a18a34c1fc78b9ba772ec37f3961b78
vivek2188 <tiwarivivekraj17@gmail.com>
2018-09-12 14:35:06 +0530

    minor

--- Algorithm/Advanced Algorithms/Fibonacci Heap/fibonacci-heap.cpp
+++ Algorithm/Advanced Algorithms/Fibonacci Heap/fibonacci-heap.cpp
@@ -46,7 +46,36 @@ class FibonacciHeap{
 		}
 		// Potential Calculator
 		void potential_function(){
-			return nodes + 2 * marked_nodes;
+			cout << nodes + 2 * marked_nodes << "\n";
+		}
+		// Insertion
+		void fib_heap_insert(int key){
+			Node *x = createnode(key);
+			if(head==NULL)
+				head = x;
+			else{
+				// Insert new node to root list
+				Node *l1 = head->left;
+				Node *l2 = x->left;
+				l1->right = x;
+				x->left = l1;
+				l2->right = head;
+				head->left = l2;
+				// Head Updation
+				if(head->key > x->key)
+					head = x;
+			}
+			nodes += 1;
+		}
+		// Print root list
+		void print_root_list(){
+			Node *h = head;
+			cout << "Root list: ";
+			do{
+				cout << h->key << " ";
+				h = h->right;
+			}while(h!=head);
+			cout << "\n";
 		}
 };
 
@@ -55,5 +84,14 @@ int main(){
 	Node *head = obj.minimum();
 	if(head==NULL)
 		cout << "Empty\n";
+	obj.fib_heap_insert(5);
+	obj.fib_heap_insert(2);
+	obj.fib_heap_insert(3);
+	obj.fib_heap_insert(1);
+	obj.fib_heap_insert(4);
+	obj.fib_heap_insert(6);
+	head = obj.minimum();
+	cout << head->key << "\n";
+	obj.print_root_list();
 	return 0;
 }
