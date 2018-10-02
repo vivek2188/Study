e387e970d18838224235c65a2c40562dccae85fd
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-02 13:44:37 +0530

    Producer code

---
+++ Operating-Systems/Processes/Posix Shared Memory/producer.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <fcntl.h>	// Provides file control options: O_RDWR, O_WRONLY, O_RDONLY etc.
+#include <stdlib.h>
+
+int main(){
+	// the size of shared memory object
+	const int SIZE = 4096;
+	// Name of the shared memory object
+	const char *name = "Shary";
+	// String to bes written to shared memory
+	const char *m0 = "Hello";
+	const char *m1 = "World";
+	// Shared memory file descriptor
+	int shm_fd;
+	// Pointer to the shared memory object
+	void *ptr;
+	// Create shared memory object
+	shm_fd = shm_open(name,O_CREAT | O_RDWR,0666);
+	// Configure the shared memory object
+	ftruncate(shm_fd,SIZE);
+	// Momory map to the shared memory object
+	ptr = mmap(0,SIZE,PROT_WRITE,MAP_SHARED,shm_fd,0);
+	// Writing to the shared memory
+	sprintf((char*)ptr,"%s",m0);
+	ptr += strlen(m0);
+	sprintf(ptr,"%s",m1);
+	ptr += strlen(m1);
+	return 0;
+}
+
+// Compile with: gcc producer.c -o producer -lrt
