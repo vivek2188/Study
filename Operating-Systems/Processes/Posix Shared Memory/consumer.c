7701f21b782b2d7a169022dee1eba4b7cf82c653
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-02 13:56:18 +0530

    Consumer code

--- Operating-Systems/Processes/Posix Shared Memory/consumer.c
+++ Operating-Systems/Processes/Posix Shared Memory/consumer.c
@@ -11,24 +11,20 @@ int main(){
 	const int SIZE = 4096;
 	// Name of the shared memory object
 	const char *name = "Shary";
-	// String to bes written to shared memory
-	const char *m0 = "Hello";
-	const char *m1 = "World";
 	// Shared memory file descriptor
 	int shm_fd;
 	// Pointer to the shared memory object
 	void *ptr;
 	// Create shared memory object
-	shm_fd = shm_open(name,O_CREAT | O_RDWR,0666);
+	shm_fd = shm_open(name,O_RDONLY,0666);
 	// Configure the shared memory object
 	ftruncate(shm_fd,SIZE);
 	// Momory map to the shared memory object
-	ptr = mmap(0,SIZE,PROT_WRITE,MAP_SHARED,shm_fd,0);
-	// Writing to the shared memory
-	sprintf(ptr,"%s",m0);
-	ptr += strlen(m0);
-	sprintf(ptr,"%s",m1);
-	ptr += strlen(m1);
+	ptr = mmap(0,SIZE,PROT_READ,MAP_SHARED,shm_fd,0);
+	// Reading the shared memory object	
+	printf("%s\n",(char*)ptr);
+	// Remove the shared memory object
+	shm_unlink(name);
 	return 0;
 }
 
