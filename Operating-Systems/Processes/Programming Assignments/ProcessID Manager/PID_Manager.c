9cadcb3500063b38bf6b7e683898710f0b5c1671
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-06 15:22:52 +0530

    defined the header file functions

---
+++ Operating-Systems/Processes/Programming Assignments/ProcessID Manager/PID_Manager.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "PID_Manager.h"
+
+int allocate_map(void){
+	// Initialising the data structure for representing PIDs
+	for(int i=0;i<size;i++)
+		map[i] = 0;
+	return 1;
+}
+
+int allocate_pid(void){
+	// Allocating a pid
+	int j;
+	for(j=0;j<size;j++){
+		if(map[j]==0){
+			map[j] = 1;
+			return (j + MIN_PID);
+		}
+	}
+	// All PIDs in use
+	return 1;
+}
+
+void release_pid(int pid){
+	// Release a pid
+	map[pid-MIN_PID] = 0;
+}
