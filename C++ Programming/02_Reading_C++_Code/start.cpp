dba38f1720dc2ad606a1c2eca1fa08cb1bbf2d97
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-11 23:39:05 +0530

    reading the integers to set min and max

--- C++ Programming/02_Reading_C++_Code/start.cpp
+++ C++ Programming/02_Reading_C++_Code/start.cpp
@@ -5,6 +5,21 @@
 using namespace std;
 
 int main(void){
-	
+	int min(numeric_limits<int>::max());
+	int max(numeric_limits<int>::min());
+	bool entered = false;
+	// Getting user input
+	int x;
+	while(cin >> x){
+		entered = true;
+		if(x<min)
+			min = x;
+		if(x>max)
+			max = x;
+	}
+	if(entered==false)
+		cout << "Input not provided\n";
+	else
+		cout << "Min: " << min << "\n" << "Max: " << max << "\n";
 	return 0;
 }
