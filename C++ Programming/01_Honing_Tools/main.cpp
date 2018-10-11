e4e497c0e2ef3f771e1768c98bc48e8aec651f63
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-11 21:37:24 +0530

    read from the desired location

--- C++ Programming/01_Honing_Tools/main.cpp
+++ C++ Programming/01_Honing_Tools/main.cpp
@@ -7,7 +7,23 @@
 #include <iterator>
 using namespace std;
 
+void read(istream &input_stream,vector<string> &text){
+	string line;
+	while(getline(input_stream,line))
+		text.push_back(line);
+}
+
 int main(int argc,char *argv[]){
-	
+	vector<string> text;
+	if(argc<2)
+		read(cin,text);
+	else{
+		ifstream in(argv[1]);	// If there is no such file than return NULL
+		if(not in){
+			perror(argv[1]);	// Prints the error message
+			return EXIT_FAILURE;
+		}
+		read(in,text);
+	}
 	return 0;
 }
