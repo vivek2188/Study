2ea8d55ebb9d56cf5d5b5f80f3690d3e3572c119
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-15 12:47:23 +0530

    echoing input to output

---
+++ C++ Programming/16_Characters/echo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+#include <ios>
+using namespace std;
+
+int main(void){
+	char ch;
+	cin >> noskipws;
+	string s = "";
+	while(cin >> ch)
+		cout << ch;
+	return 0;
+}
