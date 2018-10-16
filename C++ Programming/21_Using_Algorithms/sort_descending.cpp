a2dae87b561ea9a797ba736fadabc170eaf294b8
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-16 17:55:50 +0530

    sorting the vector in descending order

---
+++ C++ Programming/21_Using_Algorithms/sort_descending.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(int a,int b){
+	if(a>b)
+		return false;
+	return true;
+}
+
+int main(void){
+	vector<int> data;
+	// Input
+	copy(istream_iterator<int>(cin),istream_iterator<int>(),back_inserter(data));
+	// Sorting in the descending order
+	sort(data.begin(),data.end(),compare);
+	// Print
+	copy(data.begin(),data.end(),ostream_iterator<int>(cout," "));
+	cout << "\n";
+	return 0;
+}
