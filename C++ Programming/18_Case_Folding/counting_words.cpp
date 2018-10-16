f4264b788c0b5e2be21a1cba064d8cfed9f76f15
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-16 23:49:58 +0530

    handling cases

---
+++ C++ Programming/18_Case_Folding/counting_words.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <locale>
+#include <string>
+#include <vector>
+#include <map> 
+using namespace std;
+
+typedef map<string,int> count_map;
+typedef count_map::iterator count_iter;
+typedef string::size_type str_size;
+
+int main(void){
+	// Native locale
+	locale native("");
+	cin.imbue(native);
+	cout.imbue(native);
+	count_map counts;
+	string word;
+	while(cin >> word){
+		string copy;
+		for(string::iterator itr=word.begin();itr!=word.end();itr++){
+			if(isalnum(*itr,native))
+				copy.push_back(tolower(*itr,native));
+		}
+		if(not copy.empty()){
+			++counts[copy];
+			cout << copy << "\n";
+		}
+	}
+	// Printing out the map
+	for(count_iter itr=counts.begin();itr!=counts.end();++itr)
+		cout << itr->first << "\t" << itr->second << "\n";
+	return 0;
+}
