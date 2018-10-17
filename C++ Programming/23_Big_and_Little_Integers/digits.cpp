1ad5cabae988d26fa676fbd1fa5dc8881977e627
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-17 14:12:26 +0530

    minor

--- C++ Programming/23_Big_and_Little_Integers/digits.cpp
+++ C++ Programming/23_Big_and_Little_Integers/digits.cpp
@@ -9,9 +9,25 @@ int main(void){
 	if(numeric_limits<int>::is_signed)
 		cout << numeric_limits<int>::digits + 1 << "\n";
 	else cout << numeric_limits<int>::digits << "\n";
+
 	cout << "Bits for bool: ";
 	if(numeric_limits<bool>::is_signed)
 		cout << numeric_limits<bool>::digits + 1 << "\n";
 	else cout << numeric_limits<bool>::digits << "\n";
+
+	cout << "Bits for long: ";
+	if(numeric_limits<long>::is_signed)
+		cout << numeric_limits<long>::digits + 1 << "\n";
+	else cout << numeric_limits<long>::digits << "\n";
+
+	cout << "Bits for long long: ";
+	if(numeric_limits<long long>::is_signed)
+		cout << numeric_limits<long long>::digits + 1 << "\n";
+	else cout << numeric_limits<long long>::digits << "\n";
+
+	cout << "Bits for short: ";
+	if(numeric_limits<short>::is_signed)
+		cout << numeric_limits<short>::digits + 1 << "\n";
+	else cout << numeric_limits<short>::digits << "\n";
 	return 0;
 }
