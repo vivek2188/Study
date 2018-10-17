c0c15cf074f13ccffaff55911289ace75b4d47d2
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-17 12:12:42 +0530

    Palindrome checker

--- C++ Programming/21_Using_Algorithms/palindrome.cpp
+++ C++ Programming/21_Using_Algorithms/palindrome.cpp
@@ -17,18 +17,33 @@ bool non_letter(char ch){
 	return true;
 }
 
+char lower_case(char ch){
+	return tolower(ch,locale());
+}
+
+bool same_char(char a,char b){
+	if(lower_case(a) == lower_case(b))
+		return true;
+	return false;
+}
+
 bool is_palindrome(string str){
 	string::iterator new_end_iterator = remove_if(str.begin(),str.end(),non_letter);
 	string rev(str.begin(),new_end_iterator);
 	reverse(rev.begin(),rev.end());
-	cout << "Rev: " << rev << "\n";
+	if(not rev.empty() and equal(str.begin(),new_end_iterator,rev.begin(),same_char))
+		return true;
 	return false;
 }
 
 int main(void){
 	locale::global(locale(""));
 	initialize_streams();
-	string palin = "Hi, How you doing?";
-	is_palindrome(palin);
+	string palin;
+	getline(cin,palin);
+	if(is_palindrome(palin))
+		cout << "\"" << palin << "\" is a palindrome\n";
+	else 
+		cout << "\"" << palin << "\" is not a palindrome";
 	return 0;
 }
