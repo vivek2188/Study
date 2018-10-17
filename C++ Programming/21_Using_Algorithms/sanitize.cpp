97957bf345e9e7ba1e9b5dd064876b4e26170251
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-17 11:43:09 +0530

    sanitize the string using remove_if and transform functions

---
+++ C++ Programming/21_Using_Algorithms/sanitize.cpp
@@ -0,0 +1,20 @@
+#include <locale>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool non_letter(char ch){
+	if(isalnum(ch,locale()))
+		return true;
+	return false;
+}
+
+char lower_case(char ch){
+	return tolower(ch,locale());
+}
+
+string sanitize(string word){
+	// remove_if returns the valid end iterator 
+	word.erase(remove_if(word.begin(),word.end(),non_letter),word.end());
+	transform(word.begin(),word.end(),word.begin(),lower_case);
+}
