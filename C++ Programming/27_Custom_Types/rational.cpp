fb0da51c6916c3910049c4af92f2ccbd4fe1fb72
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-18 23:38:18 +0530

    accessing data members

--- C++ Programming/27_Custom_Types/rational.cpp
+++ C++ Programming/27_Custom_Types/rational.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 using namespace std;
 
 /// Defining the custom "rational" type
@@ -6,3 +7,11 @@ struct rational{
 	int numerator;		///< numerator gets the sign of the rational value
 	int denominator;	///< denominator is always positive
 };
+
+int main(void){
+	rational pi;
+	pi.numerator = 155;
+	pi.denominator = 255;
+	cout << "pi is about " << pi.numerator << "/" << pi.denominator << "\n";
+	return 0;
+}
