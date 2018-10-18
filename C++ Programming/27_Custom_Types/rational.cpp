c5d627990644519304bd7bfa3f81f3b3c7fc7c0e
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-19 00:00:04 +0530

    assign member function

--- C++ Programming/27_Custom_Types/rational.cpp
+++ C++ Programming/27_Custom_Types/rational.cpp
@@ -16,10 +16,19 @@ int gcd(int a,int b){
 
 /// Defining the custom "rational" type
 struct rational{
+	/// Assign the numerator and denominator, then to reduce to normal form
+	/// @param num numerator
+	/// @param deno denominator
+	void assign(int num,int deno){
+		numerator = num;
+		denominator = deno;
+		reduce();
+	}
 	/// Reduce the numerator and denominator by their gcd
 	void reduce(){
 		assert(denominator!=0);
 		int div = gcd(numerator,denominator);
+		cout << div << "\n";
 		numerator = numerator / div;
 		denominator = denominator / div;
 	}
@@ -30,8 +39,11 @@ struct rational{
 int main(void){
 	rational pi;
 	pi.numerator = 155;
-	pi.denominator = 0;
+	pi.denominator = 255;
 	pi.reduce();
 	cout << "pi is about " << pi.numerator << "/" << pi.denominator << "\n";
+	rational new_var;
+	new_var.assign(6,24);
+	cout << "new_var is about " << new_var.numerator << "/" << new_var.denominator << "\n";
 	return 0;
 }
