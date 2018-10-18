9c5c4cc6b5bd51aadf9c9ac3201134832a0fce91
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-18 16:13:23 +0530

    configured doxygen and main page created

---
+++ C++ Programming/25_Documentation/sample.cpp
@@ -0,0 +1,31 @@
+/** @file
+ * @brief Introduction to Doxygen
+ *
+ * This file is created to provide reference to
+ * various conventions and tags supported by Doxygen.
+ *
+ * Hope you find it helpful.
+*/
+
+/** @mainpage Palindromes
+  * Test input string to see whether they are palindromes.
+  *
+  * A palindrome is a string that reads the same forward and backward.
+  * To test for palindromes, this program needs to strip punctuation and
+  * other non-essential characters from the string, and compare letters 
+  * without regard to case differences.
+  *
+  * This program reads lines of text from the standard input and echos 
+  * to the standard output those lines that are palindromes
+  * 
+  * Source file: @link sample.cpp @endlink
+  *
+  * @date 18-10-2018
+  * @author Vivek Tiwari
+  * @version 1.0
+*/
+
+int main(void){
+	
+	return 0;
+}
