461ad2798d913cace6cf969bcbf9a05964ff1b73
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-18 23:32:33 +0530

    custom rational type

---
+++ C++ Programming/27_Custom_Types/rational.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+/// Defining the custom "rational" type
+struct rational{
+	int numerator;		///< numerator gets the sign of the rational value
+	int denominator;	///< denominator is always positive
+};
