0fd1ea28c6be7aa9a7f29661274627916b63c367
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-20 17:43:07 +0530

    accessing members

--- C++ Programming/31_Writing_Classes/point.cpp
+++ C++ Programming/31_Writing_Classes/point.cpp
@@ -1,5 +1,17 @@
+#include <iostream>
+#include <ostream>
+using namespace std;
 
 struct point{
-	double x;
-	double y;
+	double x;	// x coordinate
+	double y;	// y coordinate
 };
+
+int main(void){
+	point origin, unity;
+	origin.x = origin.y  = 0;
+	unity.x = unity.y = 1;
+	cout << "Origin: (" << origin.x << "," << origin.y << ")\n";
+	cout << "Unity: (" << unity.x << "," << unity.y << ")\n";
+	return 0;
+}
