a623908efd984da5a3c9593a81df5d0b715103de
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-27 22:34:26 +0530

    getting the input as per requirement from the stdin

--- Operating-Systems/Deadlocks/Programming-Assignment/bankers.cpp
+++ Operating-Systems/Deadlocks/Programming-Assignment/bankers.cpp
@@ -4,7 +4,29 @@
 #include <vector>
 using namespace std;
 
-int main(void){
+#define MAX_VAL 100
+int allocated[MAX_VAL][MAX_VAL];
+int required[MAX_VAL][MAX_VAL];
+int available[MAX_VAL];
 
+int main(void){
+	int n,m;
+	cout << "Number of processes: ";
+	cin >> n;
+	cout << "Number of resources: ";
+	cin >> m;
+	cout << "Enter the allocated resource matrix\n";
+	for(int i=0;i<n;++i)
+		for(int j=0;j<m;++j)
+			cin >> allocated[i][j];
+	cout << "Enter the max required matrix\n";
+	for(int i=0;i<n;++i)
+		for(int j=0;j<m;++j)
+			cin >> required[i][j];
+	cout << "Total number of resources: ";
+	for(int i=0;i<m;i++)
+		cin >> available[i];
+	cout << "Running the banker's algorithm...\n";
+	backtrack(n,m);
 	return 0;
 }
