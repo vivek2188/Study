5e22733ed95f14936a2dbcf468afacf1a23acc9b
vivek2188 <tiwarivivekraj17@gmail.com>
2018-10-30 23:20:02 +0530

    polynomial addition

--- Algorithm/Polynomial and FFT/polynomial.cpp
+++ Algorithm/Polynomial and FFT/polynomial.cpp
@@ -2,25 +2,63 @@
 #include <istream>
 #include <ostream>
 #include <vector>
+#include <iterator>
 using namespace std;
 
-typedef vector<int> coef;
-
 class polynomial{
-	coef a;
+	vector<int> coeff;
+	int degree;
 	public:
-		polynomial(){
-			polynomial(0);
+		/// Default Constructor
+		polynomial(): degree(-1){}
+		/// @param deg degree of the polynomial
+		polynomial(int deg): degree(deg){}
+		/// Copy Constructor
+		polynomial(polynomial const& p){
+			degree = p.degree;
+			coeff = p.coeff;
 		}
-		polynomial(int degree){
+		/// Getting the input
+		void input(){
 			cout << "Enter the coefficient vector: ";
-			int element;			
-			for(int i=0;i<=degree;i++){
+			int element;
+			for(int i=0;i<=degree;++i){
 				cin >> element;
-				a.push_back(element);
+				coeff.push_back(element);
 			}
 		}
+		void input(int deg){
+			if(deg==-1)
+				return;
+			degree = deg;
+			coeff.clear();
+			input();
+		}
+		/// Addition of two polynomials
+		polynomial add(polynomial const&);
+		/// Print the coefficient vector
+		void print(){
+			if(degree==-1){
+				cout << "Empty vector\n";
+				return;
+			}
+			cout << "Coefficient Vector: ";
+			copy(coeff.begin(),coeff.end(),ostream_iterator<int>(cout," "));
+			cout << endl;
+		}
 };
+polynomial polynomial::add(polynomial const& p){
+	if(degree!=p.degree || degree<0){
+		cout << "Degree of two polynomial must be same and >= 0.\n";
+		return polynomial(-1);
+	}
+	polynomial add_(degree);
+	for(int i=0;i<=degree;++i){
+		int data = coeff[i] + p.coeff[i];
+		add_.coeff.push_back(data);
+	}
+	return add_;
+}
 
 int main(void){
 	int degree;
