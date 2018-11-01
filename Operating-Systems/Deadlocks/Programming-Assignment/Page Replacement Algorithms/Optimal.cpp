2931507c7df856da268d00f2af000efa460e1193
vivek2188 <tiwarivivekraj17@gmail.com>
2018-11-01 23:55:59 +0530

    optimal page replacement algorithm

--- Operating-Systems/Deadlocks/Programming-Assignment/Page Replacement Algorithms/Optimal.cpp
+++ Operating-Systems/Deadlocks/Programming-Assignment/Page Replacement Algorithms/Optimal.cpp
@@ -5,8 +5,7 @@
 #include <vector>
 using namespace std;
 
-int predict(vector<int> buffer,int pages[],int j){
-	int n = sizeof(pages)/sizeof(pages[0]);
+int predict(vector<int> buffer,int pages[],int j,int n){
 	int p = 0;
 	for(int i=j;i<n and buffer.size()>1;i++){
 		vector<int>::iterator it = find(buffer.begin(),buffer.end(),pages[i]);
@@ -18,32 +17,35 @@ int predict(vector<int> buffer,int pages[],int j){
 
 int main(void){
 	int buffer_size;
+	cout << "Buffer size: ";
 	cin >> buffer_size;
 	int k,i;
+	cout << "Total number of pages: ";
 	cin >> k;
 	int page_fault = 0;
 	vector<int> buffer;
 	int pages[k];
-	cout << "Pages: ";
+	cout << "Pages are: ";
 	for(int i=0;i<k;i++)
 		cin >> pages[i];
 	int j =0;
-	while(j++<k){
-		i = pages[j-1];
+	while(j<k){
+		i = pages[j];
 		if(buffer.size()<buffer_size){
-			if(find(buffer.begin(),buffer.end(),i)!=buffer.end())
-				continue;
-				//page_fault++;
-			buffer.push_back(i);
+			if(find(buffer.begin(),buffer.end(),i)==buffer.end()){
+				page_fault++;
+				buffer.push_back(i);
+			}
 		}
 		else{
-			if(find(buffer.begin(),buffer.end(),i)!=buffer.end())
-				continue;
-			int l = predict(buffer,pages,j);
-			page_fault++;
-			buffer[find(buffer.begin(),buffer.end(),l)-buffer.begin()] = i;
+			if(find(buffer.begin(),buffer.end(),i)==buffer.end()){
+				int l = predict(buffer,pages,j,k);
+				page_fault++;
+				buffer[find(buffer.begin(),buffer.end(),l)-buffer.begin()] = i;
+			}
 		}
+		j++;
 	}
-	cout << page_fault << "\n";
+	cout << "Total number of Page Faults: " << page_fault << "\n";
 	return 0;
 }
