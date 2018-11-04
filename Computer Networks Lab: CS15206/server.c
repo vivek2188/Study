154c6ff32da3cc1da8f4374e56fa830dec0e61f9
vivek2188 <tiwarivivekraj17@gmail.com>
2018-11-04 19:52:39 +0530

    minor

--- Computer Networks Lab: CS15206/server.c
+++ Computer Networks Lab: CS15206/server.c
@@ -11,5 +11,20 @@
 int main(void){
 	// Creating client and server socket descriptor
 	int ssock, csock;
+	unsigned int len;
+	// Creating server and client socket object
+	struct sockaddr_in server, client;
+	ssock = socket(AF_INET,SOCK_STREAM,0);
+	if(ssock==-1){
+		perror("Socket not created\n");
+		return 1;
+	}
+
+	len = sizeof(struct sockaddr_in);
+
+	while(1){
+		// Write code here
+	}
+	close(ssock);
 	return 0;
 }
