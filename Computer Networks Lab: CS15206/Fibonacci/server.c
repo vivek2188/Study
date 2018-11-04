6a7612f7c60ae1119b7d27d31ba88fb27d9a0c9b
vivek2188 <tiwarivivekraj17@gmail.com>
2018-11-04 21:22:07 +0530

    minor

--- Computer Networks Lab: CS15206/Fibonacci/server.c
+++ Computer Networks Lab: CS15206/Fibonacci/server.c
@@ -16,6 +16,40 @@
 #include <arpa/inet.h>
 
 int main(void){
+	int ssock, csock;
+	unsigned int len;
 
+	struct sockaddr_in server, client;
+
+	ssock = socket(AF_INET,SOCK_STREAM,0);
+	if(ssock==-1){
+		perror("Socket not created\n");
+		return 1;
+	}
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(10000);
+	server.sin_addr.s_addr = INADDR_ANY;
+	bzero(&server.sin_zero,0);
+
+	len = sizeof(struct sockaddr_in);
+
+	if(bind(ssock,(struct sockaddr *)&server,len)==-1){
+		perror("Bind failed\n");
+		return 1;
+	}
+	if(listen(ssock,5)==-1){
+		perror("Listen failed\n");
+		return -1;
+	}
+	csock = accept(ssock,(struct sockaddr *)&client,&len);
+	if(csock==-1){
+		perror("Not accepted\n");
+		return 1;
+	}
+	while(1){
+		// Write code
+	}
+	close(ssock);
 	return 0;
 }
