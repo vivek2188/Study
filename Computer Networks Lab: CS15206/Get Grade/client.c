d3a601b5dc489cae3e1149e2efbe15dc8f7d1a67
vivek2188 <tiwarivivekraj17@gmail.com>
2018-11-04 22:29:12 +0530

    client side code

---
+++ Computer Networks Lab: CS15206/Get Grade/client.c
@@ -0,0 +1,57 @@
+// Client side code
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void){
+	int csock;
+	unsigned int len;
+
+	struct sockaddr_in client;
+
+	csock = socket(AF_INET,SOCK_STREAM,0);
+	if(csock==-1){
+		perror("Socket not created\n");
+		return 1;
+	}
+
+	client.sin_family = AF_INET;
+	client.sin_port = htons(10000);
+	client.sin_addr.s_addr = INADDR_ANY;
+	bzero(&client.sin_zero,0);
+
+	len = sizeof(struct sockaddr_in);
+
+	if(connect(csock,(struct sockaddr *)&client,len)==-1){
+		perror("Connection failed\n");
+		return 1;
+	}
+
+	int n;	// Input variable
+	int ch = 1;
+
+	while(1){
+		printf("Enter number: ");
+		scanf("%d",&n);
+		send(csock,&n,sizeof(n),0);
+		int grade;
+		recv(csock,&grade,sizeof(grade),0);
+		if(grade==-1)
+			printf("Failed\n");
+		else printf("Grade: %c\n",'A'+grade);
+		printf("Continue? ");
+		scanf("%d",&ch);
+		if(ch!=1){
+			close(csock);
+			return 0;
+		}
+	}
+	close(csock);
+	return 0;
+}
