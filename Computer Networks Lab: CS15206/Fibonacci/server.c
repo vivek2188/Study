c55631da853d0fb27ed6f83b0dc1a1c474f82ed3
vivek2188 <tiwarivivekraj17@gmail.com>
2018-11-04 21:12:21 +0530

    minor

---
+++ Computer Networks Lab: CS15206/Fibonacci/server.c
@@ -0,0 +1,21 @@
+/*
+	WAP using client server socket programming: A client enters an integer number n and server returns
+	the results Fi where 0 ≤ i ≤ n respectively. Fi denotes the Fibonacci number of ith position.
+
+	E.g., if n = 6
+	Output : F0 = 0, F1 = 1, F2 = 1, F3 = 2, F4 = 3, F5 = 5, F6 = 8
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void){
+
+	return 0;
+}
