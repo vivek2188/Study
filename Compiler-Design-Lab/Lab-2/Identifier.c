d8a41329235b75f34a7529297863c82a96a52be3
vivek2188 <tiwarivivekraj17@gmail.com>
2019-01-17 13:16:13 +0530

    minor

--- Compiler-Design-Lab/Lab-2/Identifier.c
+++ Compiler-Design-Lab/Lab-2/Identifier.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
+int compare(char *id, char *kw){
+	int len1, len2;
+	len1 = strlen(id);
+	len2 = strlen(kw);
+
+	if(len1!=len2)
+		return 0;	// Different
+	for(int i=0; i<len1; i++){
+		if(id[i]!=kw[i])
+			return 0;	// Different
+	}
+	return 1;	// Same
+}
+
 int valid(char *id){
 	printf("Identifier: %s, Length: %d\n", id, strlen(id));
 	int Valid = 1;
@@ -18,11 +32,32 @@ int valid(char *id){
 	return Valid;
 }
 
+int match_kwords(char *id, char *file){
+	FILE *fptr;
+	int MAX = 100, idx = 1, match = 0;
+
+	fptr = fopen(file, "r");
+	char line[MAX];
+	while( fgets(line,MAX,fptr) ){
+		if(compare(id, line) == 1){
+			printf("%s", line);
+			match = 1;
+			break;
+		}
+	}
+	return match;
+}
+
 int main(){
 	char id[32];
 	printf("Enter an identifier: ");
 	scanf("%s", id);
 	// Check whether identifier is valid or not
-	valid(id);
+	if(valid(id)){
+		// Check if it matches with any of the keywords
+		printf("%d", match_kwords(id, "Keywords.txt"));
+	}
+	else
+		printf("ERROR: Invalid Identifier\n");
 	return 0;
 }
