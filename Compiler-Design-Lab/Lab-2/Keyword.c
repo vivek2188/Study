56a359b016b552cdc40cd16fff01ddc75ff7a420
vivek2188 <tiwarivivekraj17@gmail.com>
2019-01-18 11:53:24 +0530

    valid keyword

---
+++ Compiler-Design-Lab/Lab-2/Keyword.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+int compare(char *id, char *kw){
+	int len1, len2;
+	len1 = strlen(id);
+	len2 = strlen(kw) - 1;
+
+	if(len1!=len2)
+		return 0;	// Different
+	for(int i=0; i<len1; i++){
+		if(id[i]!=kw[i])
+			return 0;	// Different
+	}
+	return 1;	// Same
+}
+
+int match_word(char *id, char *file){
+	FILE *fptr;
+	int MAX = 100, idx = 1, match = -1;
+
+	fptr = fopen(file, "r");
+	char line[MAX];
+	while( fgets(line,MAX,fptr) ){
+		if(compare(id, line) == 1){
+			match = idx;
+			break;
+		}
+		idx++;
+	}
+	return match;
+}
+
+int main(){
+
+	char id[32];
+	printf("Enter an keyword: ");
+	scanf("%s", id);
+	printf("-----------------------------\n");
+
+	// Check whether Keyword is valid or not
+	int matched = match_word(id, "Keywords.txt");
+	if(matched==-1){
+		printf("ERROR: Invalid Keyword\n");
+	}
+	else{
+		printf("Valid Keyword\n");
+		// Append to Symbol Table if not present
+		int present = match_word(id, "SymbolTableKeyword.txt");
+		if(present!=-1)
+			printf("-> Present at index %d\n", present);
+		else{
+			FILE *fpa = fopen("SymbolTableKeyword.txt","a");
+			fprintf(fpa, "%s\n", id);
+			fclose(fpa);
+		}
+	}
+	return 0;
+}
